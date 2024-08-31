f088f5536495ef3e1736d23cb6f3af4914c52323
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-08-31 13:53:51 +0530

    register form development started

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,6 +8,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createuser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
   INPUT = "input",
@@ -34,13 +35,15 @@ const PatientForm = () => {
   async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
     setIsLoading(true);
     try {
-      // const userData = {name,email,phone};
-      // const user = await createUser(userData);
-      // if (user) router.push(`/patients/${user.$id}/register`)}
+      const userData = {name,email,phone};
+      const user = await createuser(userData);
+      if (user) {
+        router.push(`/patients/${user.$id}/register`)
+      };
     } catch (error) {
       console.log(error);
-
     }
+    setIsLoading(false);
   }
   return (
     <Form {...form}>
