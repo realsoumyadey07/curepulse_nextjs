498154b9557982f7a95a3dae960414c2e03c2f32
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-02 21:48:06 +0530

    file uploading feature is being done

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -11,11 +11,12 @@ import { useRouter } from "next/navigation";
 import { createuser } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup } from "../ui/radio-group";
-import { Doctors, GenderOptions } from "@/constants";
+import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
 import { RadioGroupItem } from "@radix-ui/react-radio-group";
 import { Label } from "../ui/label";
 import Image from "next/image";
 import { SelectItem } from "../ui/select";
+import FileUploader from "../FileUploader";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -246,6 +247,44 @@ const RegisterForm = ({ user }: { user: User }) => {
             iconAlt="occupation"
           />
         </div>
+        <section className="space-y-6">
+          <div className="mb-9 space-y-">
+            <h2 className="sub-header">Identification and Verification</h2>
+          </div>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.SELECT}
+          control={form.control}
+          name="identificationType"
+          label="Identification type"
+          placeholder="Select an identification type"
+        >
+          {IdentificationTypes.map((type) => (
+            <SelectItem key={type} value={type}>
+              {type}
+            </SelectItem>
+          ))}
+        </CustomFormField>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="identificationNumber"
+          label="Identification number"
+          placeholder="123456789"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="occupation"
+        />
+        <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="identificationDocument"
+            label="Scanned copy of identification document"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <FileUploader/>
+              </FormControl>
+            )}
+          />
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
