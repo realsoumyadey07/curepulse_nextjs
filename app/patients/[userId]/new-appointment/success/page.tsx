e698e038547f55f8d34c429e9d84a67e7e22c453
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-08 12:28:22 +0530

    done

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -34,7 +34,7 @@ const Success = async ({ params: { userId }, searchParams }: SearchParamProps) =
           <h2 className='header mb-6 max-w-[600px] text-center'>
             Your <span className='text-green-500'>appointment request</span> hass been successfully submited!
           </h2>
-          <p className='text-white'>We'll be in touch shortly to confirm.</p>
+          <p className='text-white'>We will be in touch shortly to confirm.</p>
         </section>
         <section className='request-details'>
           <p className='text-white'>Requested appointment details: </p>
